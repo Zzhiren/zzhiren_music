32a4f629dfeb5238269a7e877018e91b59c3dcca
Zzhiren <zzhirensos@163.com>
2017-09-25 16:25:08 +0800

    singers.vue

---
+++ src/base/back-title/back-title.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="back_title">
+
+    </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+@import '~common/less/skin';
+@height: 44px;
+
+.back_title {
+    max-width: @max-width;
+    min-width: @min-width;
+    width: 100%;
+    height: @height;
+    background: @skin-green;
+    position: relative;
+}
+
+</style>
---
+++ src/components/singers/singers.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="singers">
+        <div class="types">
+
+        </div>
+        <div class="types">
+
+        </div>
+        <div class="types">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+@import '~common/less/skin';
+
+.singers {
+    max-width: @max-width;
+    min-width: @min-width;
+    width: 100%;
+    position: relative;
+    
+}
+
+</style>
